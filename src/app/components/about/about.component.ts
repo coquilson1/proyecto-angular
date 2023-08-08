76e5478d8b5712c838228ffd4caa0a08fd19ecf2
coquilson1 <i.infante.dacruz@outlook.com>
2023-08-08 17:03:09 -0500

    dando estilos

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public web: string;
+
+  constructor(){
+    this.title = "Jorge Infante";
+    this.subtitle = "Desarrollador web y programador";
+    this.web = "jorgeinfanteweb.pe"
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
