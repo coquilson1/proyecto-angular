cc5c79e21dbb4e643a5e1b697577fddf5fe760f6
coquilson1 <i.infante.dacruz@outlook.com>
2023-08-12 17:59:43 -0500

    list projects

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/app/models/project';
 import { ProjectService } from 'src/app/services/project.service';
+import { UploadService } from 'src/app/services/upload.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService] //Cargo el objeto ProjectService dentro de la propiedad providers de mi decorador
+  providers: [ProjectService, UploadService] //Cargo el objeto ProjectService dentro de la propiedad providers de mi decorador
 })
 export class CreateComponent implements OnInit{
   public title: string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _ProjectService: ProjectService
+    private _ProjectService: ProjectService,
+    private _uploadService: UploadService
   ){
     this.title = "Crear Proyecto"
     this.project = new Project('','','','',2023,'','');
     this.status = "";
+    this.filesToUpload = new Array<File>;
+
   }
 
   ngOnInit(): void {
@@ -30,8 +36,14 @@ export class CreateComponent implements OnInit{
     this._ProjectService.saveProject(this.project).subscribe(
       response => {
         if(response.project){
-          this.status = "success";
-          form.reset();
+          
+          //subir la imagen
+          this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image').then((result:any) => {
+            this.status = "success";
+            form.reset();
+            console.log(result);
+          });
+
         } else {
           this.status = "failed";
         }
@@ -41,6 +53,10 @@ export class CreateComponent implements OnInit{
         console.log(<any>error)
       }
     );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
